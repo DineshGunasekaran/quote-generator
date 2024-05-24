05613351c56cf799b8e34d915bdad52d8cdaa016
Dinesh_Gunasekaran <dgunasekaran@msystechnologies.com>
2024-05-24 20:50:50 +0530

    Media files added

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Daily quote generator API
+
+API Reference github :https://github.com/lukePeavey/quotable
+API End point: https://api.quotable.io/random - for random quotes */
+
+
+const quoteTextEle = document.getElementById('quoteEle');
+const authorTextEle = document.getElementById('authorEle');
+const apiURL = 'https://api.quotable.io/random';
+
+
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  quoteTextEle.innerHtml = data.content;
+  authorTextEle.innerHtml = data.author;
+
+
+}
+
+getQuote(apiURL);
